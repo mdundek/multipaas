824d5283f29ce9e61f74a3b770b7ad23ce07ec66
Michael Dundek <mdundek@gmail.com>
2020-06-17 11:13:30 +0200

    debugging

--- src/api/src/middleware/registry_auth.js
+++ src/api/src/middleware/registry_auth.js
@@ -20,7 +20,7 @@ module.exports = () => {
 			try {
 				
 				if(req.headers.authorization) {
-					console.log(99);
+					
 					// console.log("HEADERS =>", JSON.stringify(req.headers, null, 4));
 					
 					// --------------- PULL -------------
@@ -51,21 +51,22 @@ module.exports = () => {
 					let userCredentials = buff.toString('ascii').split(":");
 			
 					// TODO: If credentials belong to super user, then authorize everything
-					console.log(99);
+					
 					let uriRequest = req.headers["registry-call"];
 				
 					let uriArray = uriRequest.split(" ");
 					let uri = uriArray.find(o => o.indexOf("/v2/") == 0);
 					let uriSplit = uri.split("/").filter(o => o.length > 0);
-					console.log(99);
+					console.log(uriArray[0]);
+					console.log(uri);
 					if(uriArray[0] == "GET" && (uri == "/v2/_catalog" || uri == "/v2/" || uri.indexOf("/list") == (uri.length - 5) || uri.indexOf("/manifests/") != -1 )) {
 						res.status(200);
 						res.send('ok');
 					} else if(uriSplit.length < 4 || (uriSplit[4] != "blobs" && uriSplit[4] != "manifests")) {
-						console.log(3);
+						
 						throw new Error("Unauthorized");
 					} else {
-						console.log(99);
+						
 						let existingAcc = accounts.find(o => o.name == uriSplit[1]);
 						if(!existingAcc){
 							existingAcc = await DBController.getAccountByName(uriSplit[1]);
@@ -93,14 +94,14 @@ module.exports = () => {
 							existingOrg.registryUser != userCredentials[0] || 
 							decrypt(existingOrg.registryPass, existingOrg.bcryptSalt) != userCredentials[1]
 						){
-							console.log(6);
+							
 							throw new Error("Unauthorized");
 						}
 						res.status(200);
 						res.send('ok');
 					}
 				} else {
-					console.log(7);
+					
 					throw new Error("Unauthorized");
 				}
 			} catch (error) {
